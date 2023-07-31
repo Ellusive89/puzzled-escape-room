71c1c58c614143574f8b3a3907975098f388134e
Ellusive89 <>
2023-07-31 15:41:29 +0000

    create reservation model and superuser

--- escaperoom/models.py
+++ escaperoom/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+ROOM_CHOICES = (
+    ("Lost in Time", "Lost in Time"),
+    ("Enchanted Forest", "Enchanted Forest"),
+    ("Space Odyssey", "Space Odyssey"),
+    ("The Haunted Manor", "The Haunted Manor"),
+    ("Art Heist", "Art Heist"),
+    ("Survival Island", "Survival Island"),
+    ("Carnival of Curiosities", "Carnival of Curiosities"),
+    ("Egyptian Tomb", "Egyptian Tomb"),
+)
+
+TIME_CHOICES = (
+    ("9:00 AM", "9:00 AM"),
+    ("10:30 AM", "10:30 AM"),
+    ("12:00 PM", "12:00 PM"),
+    ("1:30 PM", "1:30 PM"),
+    ("3:00 PM", "3:00 PM"),
+    ("4:30 PM", "4:30 PM"),
+    ("6:00 PM", "6:00 PM"),
+    ("7:30 PM", "7:30 PM"),
+    ("9:00 PM", "9:00 PM"),
+)
+
+PLAYER_NUMBER_CHOICES = [(i, str(i)) for i in range(2, 9)]
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    room = models.CharField(
+        max_length=50, choices=ROOM_CHOICES, default="Lost in Time")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(
+        max_length=10, choices=TIME_CHOICES, default="9:00 AM")
+    players = models.IntegerField(choices=PLAYER_NUMBER_CHOICES, default=2)
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} | room: {self.room} | day: {self.day} | time: {self.time} | players: {self.players}"
--- escaperoom/views.py
+++ escaperoom/views.py
@@ -1,7 +1,117 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
+from .models import *
+from django.contrib import messages
 
 
 def escaperoom_page_view(request):
     return render(request, 'escaperoom.html')
+
+
+def reservation_page_view(request):
+    return render(request, 'reservation.html')
+
+
+def booking(request):
+    weekdays = validWeekday(31)
+    validateWeekdays = isWeekdayValid(weekdays)
+
+    if request.method == 'POST':
+        room = request.POST.get('room')
+        day = request.POST.get('day')
+        if room == None:
+            messages.success(request, "Please Select A Room!")
+            return redirect('booking')
+
+        # Store day and room in django session:
+        request.session['day'] = day
+        request.session['room'] = room
+
+        return redirect('bookingSubmit')
+
+    return render(request, 'booking.html', {
+        'weekdays': weekdays,
+        'validateWeekdays': validateWeekdays,
+    })
+
+
+def bookingSubmit(request):
+    user = request.user
+    # Adjust times based on the room schedule
+    times = ["9 AM", "10:30 AM", "12:00 PM", "1:30 PM",
+             "3:00 PM", "4:30 PM", "6:00 PM", "7:30 PM", "9:00 PM"]
+    today = datetime.now()
+    minDate = today.strftime('%Y-%m-%d')
+    deltatime = today + timedelta(days=21)
+    strdeltatime = deltatime.strftime('%Y-%m-%d')
+    maxDate = strdeltatime
+
+    # Get stored data from django session:
+    day = request.session.get('day')
+    room = request.session.get('room')
+
+    # Check availability
+    hour = checkTime(times, day)
+    if request.method == 'POST':
+        time = request.POST.get("time")
+        date = dayToWeekday(day)
+
+        if room != None:
+            if day <= maxDate and day >= minDate:
+                if Reservation.objects.filter(day=day, room=room).count() < 8:
+                    if Reservation.objects.filter(day=day, time=time, room=room).count() < 1:
+                        ReservationForm = Reservation.objects.get_or_create(
+                            user=user,
+                            room=room,
+                            day=day,
+                            time=time,
+                        )
+                        messages.success(request, "Booking Saved!")
+                        return redirect('index')
+                    else:
+                        messages.success(
+                            request, "The Selected Time Has Been Reserved Before!")
+                else:
+                    messages.success(request, "The Selected Day Is Full!")
+            else:
+                messages.success(
+                    request, "The Selected Date Isn't In The Correct Time Period!")
+        else:
+            messages.success(request, "Please Select A Room!")
+
+    return render(request, 'bookingSubmit.html', {
+        'times': hour,
+    })
+
+
+def dayToWeekday(x):
+    z = datetime.strptime(x, "%Y-%m-%d")
+    y = z.strftime('%A')
+    return y
+
+
+def validWeekday(days):
+    today = datetime.now()
+    weekdays = []
+    for i in range(0, days):
+        x = today + timedelta(days=i)
+        y = x.strftime('%A')
+        if y == 'Tuesday' or y == 'Wednesday' or y == 'Thursday' or y == 'Friday' or y == 'Saturday' or y == 'Sunday':
+            weekdays.append(x.strftime('%Y-%m-%d'))
+    return weekdays
+
+
+def isWeekdayValid(x):
+    validateWeekdays = []
+    for j in x:
+        if Reservation.objects.filter(day=j).count() < 10:
+            validateWeekdays.append(j)
+    return validateWeekdays
+
+
+def checkTime(times, day):
+    x = []
+    for k in times:
+        if Reservation.objects.filter(day=day, time=k).count() < 1:
+            x.append(k)
+    return x
