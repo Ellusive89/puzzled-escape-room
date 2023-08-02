a4465ee4056782dfd8b802709c76bbbb74464e10
Ellusive89 <>
2023-08-02 14:32:01 +0000

    add content to user panel template

--- escaperoom/urls.py
+++ escaperoom/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.escaperoom_page_view, name='escaperoom'),
+    path('escaperoom.html', views.escaperoom_page_view, name='escaperoom'),
     path('booking', views.booking, name='booking'),
     path('booking-submit', views.bookingSubmit, name='bookingSubmit'),
     path('our-rooms', views.our_rooms, name='our_rooms'),
