1755de46d16e8e94bc7fac67a120e8dbdae4f7dd
Ellusive89 <>
2023-08-04 10:54:24 +0000

    Add 4 more rooms descriptions to Our Rooms page

--- puzzled/settings.py
+++ puzzled/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-ellusive89-puzzled-escap-ml53o3bujs.us2.codeanyapp.com", "puzzled-escaperoom-0a01ce260114.herokuapp.com"]
 
